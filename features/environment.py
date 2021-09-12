aebe2a8f2079d06947d9dacffd3cb514a30ca301
Barracudakun <yingxukun@yahoo.com>
2021-09-12 16:12:44 -0500

    update search item

--- features/environment.py
+++ features/environment.py
@@ -5,7 +5,7 @@ def browser_init(context):
     """
     :param context: Behave context
     """
-    context.driver = webdriver.Chrome()
+    context.driver = webdriver.Chrome(executable_path="/Users/seashore/PycharmProjects/Careerist7/python-selenium-automation/chromedriver 2")
     # context.browser = webdriver.Safari()
     # context.browser = webdriver.Firefox()
 
